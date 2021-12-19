ad7c69396675629f9046a52329eed4de99548443
mrvalraven <akatiago@gmail.com>
2021-12-19 00:08:01 +0000

    added truths

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,50 @@
 </template>
 
 <style lang="scss">
+@import url("https://use.fontawesome.com/releases/v5.15.2/css/all.css");
+@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+$whiteBlue: #e7ecef;
+$darkBlue: #274c77;
+$normalBlue: #6aa7cf;
+$lightBlue: #a3cef1;
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Poppins", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  cursor: default;
+  overflow: hidden;
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+body {
+  &::-webkit-scrollbar {
+    width: 0.5rem;
   }
+  &::-webkit-scrollbar-track {
+    background-color: $whiteBlue;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: $darkBlue;
+    border-radius: 5px;
+  }
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+a {
+  text-decoration: none;
+  cursor: pointer;
+  opacity: 0.75;
+  color: #2c3e50;
+  transition: color 0.25s ease-in-out;
+
+  &:hover {
+    opacity: 1;
+  }
+}
+nav,
+ul,
+li {
+  list-style-type: none;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,121 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="container">
+    <div class="card-container" v-if="currentStep === 'mainMenu'">
+      <h1>Sapatuna</h1>
+      <h2>O melhor party game forévora</h2>
+      <div class="button-container">
+        <button @click="currentStep = 'playerMenu'">Jogar</button>
+        <button>Donate 💓</button>
+      </div>
+    </div>
+    <div class="card-container" v-else-if="currentStep === 'playerMenu'">
+      <h1>Jogadores</h1>
+      <ul>
+        <li v-for="jogador in jogadores" :key="jogador">{{ jogador }}</li>
+      </ul>
+      <div class="input-container">
+        <label for="jogador">Adicionar novo jogador</label>
+        <input type="text" v-model="addingJogador" />
+        <button @click="addJogador">Adicionar</button>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  data() {
+    return {
+      currentStep: "playerMenu",
+      jogadores: [],
+      addingJogador: "",
+    };
+  },
+  created() {
+    const currentJogadores = localStorage.getItem("theme");
+    this.checkJogadores(currentJogadores);
+  },
+  methods: {
+    addJogador() {
+      if (this.addingJogador === "") {
+        return;
+      }
+
+      this.jogadores.push(this.addingJogador);
+      this.addingJogador = "";
+      localStorage.setItem("jogadores", this.jogadores);
+      console.log(this.jogadores);
+    },
+    checkJogadores(jogadores) {
+      localStorage.setItem("jogadores", this.jogadores);
+    },
+    checkTheme(theme) {
+      if (theme === null) {
+        this.theme = "light";
+      } else {
+        this.theme = theme;
+      }
+
+      if (this.theme === "light") {
+        this.isDarkMode = false;
+      } else {
+        this.isDarkMode = true;
+      }
+    },
+
+    toggleDarkMode() {
+      this.theme === "light" ? (this.theme = "dark") : (this.theme = "light");
+      this.checkTheme(this.theme);
+      // Finally, let's save the current preference to localStorage to keep using it
+      localStorage.setItem("theme", this.theme);
+    },
+  },
+  components: {},
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  margin: 0;
+  min-height: 100vh;
+  background-color: #ffffff;
+  color: black;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .card-container {
+    width: auto;
+    max-width: 900px;
+    padding: 20px 30px;
+    border-radius: 10px;
+    background-color: rgba(255, 255, 255, 0.4);
+    box-shadow: 0 10px 10px 10px rgba(0, 0, 0, 0.2);
+
+    .button-container {
+      display: flex;
+      justify-content: space-evenly;
+      margin-top: 20px;
+
+      button {
+        outline: none;
+        border: 1px solid black;
+        border-radius: 12px;
+        padding: 10px 20px;
+        width: 100px;
+
+        &:hover {
+          cursor: pointer;
+        }
+      }
+    }
+
+    .input-container {
+      display: flex;
+      flex-direction: column;
+    }
   }
 }
-</script>
+</style>
